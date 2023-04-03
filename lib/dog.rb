8644202e31997d42387d2ec54ddb89f5a9eb9ddf
alex lieberman <alex.lieberman182@gmail.com>
2023-04-03 10:54:52 -0700

    commit tree 8d60de8526a2c8e0d65f01a42b44856d2846f51a

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,20 @@
-class Dog
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/test.sqlite3"
+)
+
+sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+)
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+class Dog < ActiveRecord::Base
 end
